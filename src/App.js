b925bc97b8ba75e40a5d4054a0f131e7c0d79bf9
L7R-a <gedylarosa@gmail.com>
2024-08-24 14:03:29 -0400

    Trying to style the sidecars

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
+// import "primereact/resources/themes/saga-blue/theme.css"; 
+import "primereact/resources/themes/soho-dark/theme.css"; 
+import "./index.css";
+
 
 const Main = () => {
   return (
--- src/components/NodeDetails.js
+++ src/components/NodeDetails.js
@@ -3,38 +3,37 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { TabView, TabPanel } from 'primereact/tabview';
-import './NodeDetails.module.css'; // Custom CSS file
+import styles from './NodeDetails.module.css'; // Import CSS module
 import LettersTable from './SidecarContent/LetterTable';
 import Relationships from './SidecarContent/Relationships';
 import OpenData from './SidecarContent/OpenData';
 import Biography from './SidecarContent/Biography';
 
-const NodeDetails = ({ nodeData , handleNodeClick}) => {
-  // style={{backgroundColor: 'red'}}
+const NodeDetails = ({ nodeData, handleNodeClick }) => {
   return (
-    <div> 
-        <TabView >
-            <TabPanel key={"Biography"} header={"Biography"}>
-              <div>
-                <Biography nodeData={nodeData}/>
-              </div>
-            </TabPanel>
-            <TabPanel key={"Letters"} header={"Letters"}>
-              <div>
-                <LettersTable nodeData={nodeData} />
-              </div>
-            </TabPanel>
-            <TabPanel key={"Relationships"} header={"Relationships"}>
-              <div>
-                <Relationships nodeData={nodeData} handleNodeClick={handleNodeClick}/>
-              </div>
-            </TabPanel>
-            <TabPanel key={"Open Data"} header={"Open Data"}>
-              <div>
-                <OpenData nodeData={nodeData} />
-              </div>
-            </TabPanel>
-        </TabView>
+    <div className={styles.sidecarContainerFull}>
+      <TabView>
+        <TabPanel key={"Biography"} header={"Biography"}>
+          <div>
+            <Biography nodeData={nodeData} />
+          </div>
+        </TabPanel>
+        <TabPanel key={"Letters"} header={"Letters"}>
+          <div>
+            <LettersTable nodeData={nodeData} />
+          </div>
+        </TabPanel>
+        <TabPanel key={"Relationships"} header={"Relationships"}>
+          <div>
+            <Relationships nodeData={nodeData} handleNodeClick={handleNodeClick} />
+          </div>
+        </TabPanel>
+        <TabPanel key={"Open Data"} header={"Open Data"}>
+          <div>
+            <OpenData nodeData={nodeData} />
+          </div>
+        </TabPanel>
+      </TabView>
     </div>
   );
 };
