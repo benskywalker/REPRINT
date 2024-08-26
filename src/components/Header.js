94c4a95673c43a25deba67baf16e0c168c89f9c5
Gabriel <ga612326@ucf.edu>
2024-08-26 17:53:38 -0400

    Basic gallery done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,78 @@
 // Header component for ui
 
-import React from 'react'
-import { Button } from 'primereact/button'
-import styles from './Header.module.css'
-import { InputText } from 'primereact/inputtext'
-import { Sidebar } from 'primereact/sidebar'
-import 'primereact/resources/themes/saga-blue/theme.css'
-import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Button } from "primereact/button";
+import styles from "./Header.module.css";
+import { InputText } from "primereact/inputtext";
+import { Sidebar } from "primereact/sidebar";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-const Header = ({ onUploadClick }) => {
-  const [sidebarVisible, setSidebarVisible] = useState(false)
+const Header = ({ onUploadClick, onSearchChange, gallery = false }) => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
 
+  const handleInputChange = (event) => {
+    onSearchChange(event.target.value);
+  };
   const toggleSidebar = () => {
-    setSidebarVisible(!sidebarVisible)
-  }
+    setSidebarVisible(!sidebarVisible);
+  };
   return (
     <div className={styles.header}>
-      <NavLink to="/" activeclassname={styles.activeLink} className={styles.title}>Network Analysis</NavLink>
-      {/* search bar in middle of header */}
-      <InputText placeholder='Search' className={styles.search} />
+      <NavLink
+        to="/"
+        activeclassname={styles.activeLink}
+        className={styles.title}
+      >
+        Network Analysis
+      </NavLink>
+      {gallery ? (
+        <InputText
+          placeholder="Search"
+          className={styles.search}
+          onChange={handleInputChange}
+        />
+      ) : (
+        <InputText placeholder="Search" className={styles.search} />
+      )}
       {/* pi bar menu for sidebar pop up */}
-      <Button icon="pi pi-bars" className={styles.menu} onClick={toggleSidebar} />
-      <Sidebar visible={sidebarVisible} position="right" onHide={() => setSidebarVisible(false)}>
+      <Button
+        icon="pi pi-bars"
+        className={styles.menu}
+        onClick={toggleSidebar}
+      />
+      <Sidebar
+        visible={sidebarVisible}
+        position="right"
+        onHide={() => setSidebarVisible(false)}
+      >
         <h3>PRINT Network Analysis</h3>
         <hr></hr>
         {/* display routes */}
         <ul className={styles.routesList}>
-            <li><NavLink to="/" activeclassname={styles.activeLink}>Home</NavLink></li>
-            <li><NavLink to="/query-tool" activeclassname={styles.activeLink}>Query Tool</NavLink></li>
-            <li><NavLink to="/gallery" activeclassname={styles.activeLink}>Gallery</NavLink></li>
+          <li>
+            <NavLink to="/" activeclassname={styles.activeLink}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/query-tool" activeclassname={styles.activeLink}>
+              Query Tool
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/gallery" activeclassname={styles.activeLink}>
+              Gallery
+            </NavLink>
+          </li>
         </ul>
         <hr></hr>
       </Sidebar>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
