e2c67c5172352e19ea7c9c9053acbfe44feadb22
L7R-a <gedylarosa@gmail.com>
2024-09-02 22:21:58 -0400

    Added a placeholder image for the portraits of the biography section

--- src/components/SidecarContent/Biography.js
+++ src/components/SidecarContent/Biography.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Image } from 'primereact/image';
+import Logo from '../images/logo.png'; // Import the logo image
 
 const Biography = (nodeData) => (
-  <div className="sidecarBody">
+  <div>
     <div className="biographyContent">
-        {nodeData?.data?.image ? <Image src={nodeData.data.image} className='bioImage'/> : null}
+        {nodeData?.data?.image ? <Image src={nodeData.data.image} className='bioImage'/> :         <Image src={Logo} alt='Logo' />
+      }
     </div>
     <p>Detailed biography content goes here... Detailed biography content goes here...Detailed biography content goes here...Detailed biography content goes here...</p>
     <p>Detailed biography content goes here...</p>
