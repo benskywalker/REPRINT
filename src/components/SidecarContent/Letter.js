76b5c7c6ef8bb4d99b22fffb25c3f2035a2015d7
L7R-a <gedylarosa@gmail.com>
2024-09-02 22:12:59 -0400

    Added images to the letter component and also fixed a css problem

--- src/components/SidecarContent/Letter.js
+++ src/components/SidecarContent/Letter.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Image } from 'primereact/image';
 import { Splitter, SplitterPanel } from 'primereact/splitter';
 import './Letter.css'; // Make sure to import the CSS file
+import Logo from '../images/logo.png'; // Import the logo image
 
 const Letter = ({ id }) => {
   const [isClosed, setIsClosed] = useState(false);
@@ -18,9 +19,11 @@ const Letter = ({ id }) => {
     <Splitter className='letterSplitter' onResizeEnd={handleResizeEnd}>
       <SplitterPanel className={`flex align-items-center justify-content-center letterSplitterPanel ${isClosed ? 'closed' : ''}`} minSize={0}>
         <p>Letter {id}</p>
+        <Image src={Logo} alt='Logo' />
       </SplitterPanel>
-      <SplitterPanel className={`flex align-items-center dockPanel letterSplitterPanel ${isClosed ? 'closed' : ''}`} style={{ flexDirection: 'column' }} minSize={0}>
+      <SplitterPanel className={`flex align-items-center  justify-content-center letterSplitterPanel ${isClosed ? 'closed' : ''}`} style={{ flexDirection: 'column' }} minSize={0}>
         <p>Transcription {id}</p>
+        <Image src={Logo} alt='Logo' className='letterImage'/>
       </SplitterPanel>
     </Splitter>
   );
