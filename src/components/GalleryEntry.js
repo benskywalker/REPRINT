755a26322ded7eb438696ddf5ff7b31b3827c756
Bautista Solanet <145712564+Bsolanet@users.noreply.github.com>
2024-09-16 17:23:58 -0400

    I hate CSS so so so much

--- src/components/GalleryEntry.js
+++ src/components/GalleryEntry.js
@@ -6,7 +6,7 @@ const GalleryEntry = ({ image, name }) => {
     const bio = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
 
     return (
-        <Card>
+        <Card className="gallery-card">
             <img src={image} alt={name} className = 'gallery-image'/>
             <div className="gallery-text">
                 <div className="gallery-title">{name}</div>
