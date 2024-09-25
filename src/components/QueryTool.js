2c3b44ce2c9fe531049124272d1eeab74f93dea7
Bautista Solanet <145712564+Bsolanet@users.noreply.github.com>
2024-09-25 19:33:15 -0400

    Started work on new Query tool
    Started making query tool that looks like the one provided by PRINT, still needs CSS and functionality to add more clauses

--- src/components/QueryTool.js
+++ src/components/QueryTool.js
@@ -1,9 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 
 import { Button } from "primereact/button";
+import { AutoComplete } from "primereact/autocomplete";
 import { ContextMenu } from "primereact/contextmenu";
+import { Dropdown } from "primereact/dropdown";
+import { Calendar } from "primereact/calendar";
 
 import { QueryClause } from "./QueryClause";
 
@@ -22,18 +25,21 @@ data.person.map((person) => {
 });
 
 data.keyword.map((keyword) => {
-  return suggestions.push(keyword);
+  return suggestions.push(keyword.keyword);
 });
 
 data.occupation.map((occupation) => {
-  return suggestions.push(occupation);
+  return suggestions.push(occupation.occupationDesc);
 });
 
 data.organizationtype.map((organization) => {
-  return suggestions.push(organization.organizationName);
+  return suggestions.push(organization.organizationDesc);
 });
 
+const placeItems=[];
+
 data.place.map((place) => {
+  placeItems.push({ label: place.placeNameStd, value: place.placeNameStd });
   return suggestions.push(place.placeNameStd);
 });
 
@@ -56,16 +62,73 @@ data.role.map((role) => {
   
 });
 
+const fields = [
+  { label: "Person", code: "person" },
+  { label: "Place", code: "place" },
+  { label: "Religion", code: "religion" },
+  { label: "Occupation", code: "occupation" },
+  { label: "Organization", code: "organization" },
+  { label: "Mention", code: "mention" },
+  { label: "Keyword", code: "keyword" },
+  { label: "Repository", code: "repository" },
+]
+
+const minDate = new Date();
+minDate.setFullYear(1600, 0, 1);
+
+const maxDate = new Date();
+maxDate.setFullYear(1750, 0, 1);
+
+
 const QueryTool = () => {
   const [query, setQuery] = useState([]);
   const [selectedClause, setSelectedClause] = useState("");
-  const [clauses, setClauses] = useState([uuidv4()]);
+  const [clauses, setClauses] = useState([]);
+  const [value, setValue] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [selectedField, setSelectedField] = useState("");
+  const [selectedRole, setSelectedRole] = useState("");
+  const [roleItems, setRoleItems] = useState([]);
+  const [startDate, setStartDate] = useState(minDate);
+  const [endDate, setEndDate] = useState(maxDate);
   const cm = useRef(null);
 
   const cmItems = [
     { label: "Remove", icon: "pi pi-times", command: (e) => {removeClause()}}
   ];
+
+  useEffect(() => {
+    console.log(selectedField);
+    if(selectedField === "Person") {
+      const items = [
+        { label: "First Name", value: "firstName" },
+        { label: "Middle Name", value: "middleName" },
+        { label: "Last Name", value: "lastName"},
+        { label: "Full Name", value: "fullName"},
+        { label: "Author", value: "author"},
+        { label: "Recipient", value: "recipient"}
+      ];
+
+      setRoleItems(items);
+    } else if(selectedField === "Place") {
+      const items = [
+        { label: "Sent", value: "sent" },
+        { label: "Received", value: "received" },
+        { label: "Mentioned", value: "mentioned" }
+      ];
+
+      setRoleItems(items);
+    }}, []);
   
+  const searchSuggestions = (e) => {
+    const query = e.query;
+    let filtered = [];
+    let filteredSuggestions = suggestions.filter((suggestion) => {
+      return suggestion.toLowerCase().includes(query.toLowerCase());
+    });
+    
+    setFilteredSuggestions(filteredSuggestions);
+  };
 
   const onAddClause = () => {
     const id = uuidv4();
@@ -84,45 +147,78 @@ const QueryTool = () => {
     setClauses(newClause);
   }
 
-  const setQueries = (q, index) => {
-    setQuery(prevQuery => {
-      const newQuery = [...prevQuery];
-      newQuery[index] = q;
-      return newQuery;
-    })
-  }
-
-  const onSubmit = () => {
-    const queries = [];
-    query.map((q) => {
-      q.map((clause) => {
-        queries.push(clause);
-      })
-    })
-    console.log(queries);
-  }
-
-  return(
+  return (
     <div className="query-tool">
-      <ContextMenu model={cmItems} ref={cm}/>
       <div className="buttons">
-        <Button className="add-button" label = "Add Clause" onClick={onAddClause}/>
-        <Button className="submit-button" label="Submit" onClick={onSubmit}/>
+        <Button className="add-button" label="Add Clause" onClick={onAddClause} />
+        <Button className="search-button" label="Search" />
       </div>
-      <div className="query-items">
-        {clauses.map((clause, index) => (
-            <QueryClause
-              className="query-clause" 
-              key={clause.id} 
-              setQuery={setQueries} 
-              index={index} 
-              suggestions={suggestions} 
-              onRightClick={onRightClick}
+      <ContextMenu model={cmItems} ref={cm} />
+      <h2>Advanced Search</h2>
+      <h3>Construct your Search:</h3>
+      <div className="query-clauses">
+        {/* Base clause that can't be removed*/}
+        <div className="base-clause">
+          <div className="term">
+            <div className="inputTitle">Term(s)</div> 
+            <AutoComplete 
+              className="query-drop"
+              placeholder="Term(s)"
+              multiple 
+              value={value} 
+              suggestions={filteredSuggestions} 
+              completeMethod={searchSuggestions} 
+              onChange={setValue}
             />
-        ))}
+          </div>
+          <div className="fields">
+            <div className = "inputTitle">Field</div>
+            <Dropdown
+              className="query-drop"
+              placeholder="Field"
+              options={fields}
+              value={selectedField}
+              onChange={(e) => setSelectedField(e.value)}
+              optionLabel="label"
+            />
+          </div>
+          { (selectedField.code === "person" || selectedField.code === "place") && 
+            <div className="place">
+              <div className="inputTitle">Role</div>
+              <Dropdown
+                className="query-drop"
+                placeholder="Role"
+                options={roleItems}
+                value={selectedRole}
+                onChange={(e) => setSelectedRole(e.value)}
+              />
+            </div>
+          }
+        </div>  
+      </div>
+      <div className="search-dates">
+          <div className="inputTitle">Start date</div>
+          <Calendar 
+            className="query-drop" 
+            placeholder="Start date" 
+            value={startDate}
+            onChange={(e) => setStartDate(e.value)}
+            minDate={minDate}
+            maxDate={maxDate}
+          />
+          <div className="inputTitle">End date</div>
+          <Calendar 
+            className="query-drop" 
+            placeholder="End date" 
+            value={endDate}
+            onChange={(e) => setEndDate(e.value)}
+            minDate={minDate}
+            maxDate={maxDate}
+          />
       </div>
     </div>
-  );
+  )
+  
 }
 
 export default QueryTool;
