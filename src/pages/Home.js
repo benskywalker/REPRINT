974eaaff258a2980d19c13868a55a963e9eaa7f6
L7R-a <gedylarosa@gmail.com>
2024-10-21 20:31:59 -0400

    Improved css of query-tool table (still in progress)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -325,6 +325,7 @@ const Home = ({ searchQuery }) => {
               severity={showEdges ? "success" : "danger"}
             />
             <DataTable
+              className="Home-DataTable"
               value={selectedNodes}
               reorderableRows
               onRowReorder={onRowReorder}
