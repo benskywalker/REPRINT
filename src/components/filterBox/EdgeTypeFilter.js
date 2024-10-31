afe2618a5bbba9248bf3f14f82840e7aaba6967d
nickaking02@gmail.com <nickaking02@gmail.com>
2024-10-31 00:07:11 -0400

    Update EdgeTypeFilter component styles; change position to absolute, increase padding and font size, and rename header to 'Filter Connections'

--- src/components/filterBox/EdgeTypeFilter.js
+++ src/components/filterBox/EdgeTypeFilter.js
@@ -20,18 +20,20 @@ const EdgeTypeFilter = ({ onChange }) => {
 
   return (
     <div
-      style={{
-        position: "fixed",
-        bottom: "10px",
-        right: "10px",
-        background: "white",
-        color: "black",
-        padding: "10px",
-        borderRadius: "8px",
-        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-      }}
+    style={{
+      position: "absolute",
+      bottom: "10px",
+      right: "10px",
+      background: "white",
+      color: "black",
+      padding: "20px", // Increased padding for more space
+      borderRadius: "8px",
+      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+      fontSize: "16px", // Increased font size
+      lineHeight: "1.8", // Increased line height for more space between lines
+    }}
     >
-      <h4>Filter Edge Types</h4>
+      <h4>Filter Connections</h4>
       {edgeTypes.map((type) => (
         <div key={type} className="p-field-checkbox">
           <Checkbox
