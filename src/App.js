b5a70cf3e1aa7e6f4f4c3a882010a89eb9f81d64
IdyllicDawn <89504852+IdyllicDawn@users.noreply.github.com>
2025-02-23 20:47:51 -0500

    Small fix to merge to have it redirect to home page properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Gallery from "./pages/Gallery/Gallery";
 import Header from "./components/header/Header";
 import Home from "./pages/Home/Home";
@@ -9,7 +9,7 @@ import "primeflex/primeflex.css";
 import "./App.css";
 import QueryTool from "./pages/QueryTool/QueryTool";
 
-const Main = ({searchQuery}) => {
+const Main = ({ searchQuery }) => {
 
   return <Home className="home" searchQuery={searchQuery} />;
 };
@@ -25,13 +25,14 @@ const App = () => {
     <Router >
       <Header onSearchChange={handleSearchChange} />
       <Routes>
+        <Route path="/" element={<Navigate to="/REPRINT" replace />} />
         <Route path="/REPRINT" element={<Main searchQuery={searchQuery} />} />
         <Route
           path="/REPRINT/gallery"
           element={<Gallery searchQuery={searchQuery} />}
         />
         <Route path="/REPRINT/query-tool" element={<QueryTool />} />
-        <Route path="/REPRINT/*" element={<Home  />} />
+        <Route path="/REPRINT/*" element={<Home />} />
       </Routes>
     </Router>
   );
